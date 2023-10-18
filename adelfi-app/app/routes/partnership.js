a3f8c53ed5bce483245c5c7de225c5e0a62b47d7
michael-glum <glummb@miamioh.edu>
2023-10-18 14:19:20 -0400

    Additional Defer testing - post db working

--- adelfi-app/app/routes/partnership.js
+++ adelfi-app/app/routes/partnership.js
@@ -22,73 +22,73 @@ export const action = async ({ request }) => {
                 return json({ message: 'Partnerships not found' }, { status: 404 });
             }
         
-            await db.partnership.updateMany({ where: { shop: "quickstart-9f306b3f.myshopify.com" }, data: { totalSales: 1.0 }});
-        } //extra
-    } //extra
-    //         const updateResponses = []
+            //await db.partnership.updateMany({ where: { shop: "quickstart-9f306b3f.myshopify.com" }, data: { totalSales: 1.0 }});
+        //} //extra
+    //} //extra
+            const updateResponses = []
 
-    //         updateResponses.push(partnerships.forEach(async function(partnership) {
-    //             if (partnership.discountId != null && partnership.totalSales != null && partnership.currSales != null) {
-    //                 const { admin } = await unauthenticated.admin(partnership.shop);
-    //                 const response = await admin.graphql(
-    //                     `#graphql
-    //                     query queryOrders($id: ID!) {
-    //                         orders(query: "created_at:2023-10-16") {
-    //                             edges {
-    //                                 node {
-    //                                     discountCodes
-    //                                     netPaymentSet
-    //                                     createdAt
-    //                                 }
-    //                             }
-    //                         }
-    //                     }
-    //                     `,
-    //                     {
-    //                         variables: {
-    //                         },
-    //                     }
-    //                 );
+            updateResponses.push(partnerships.forEach(async function(partnership) {
+                if (partnership.discountId != null && partnership.totalSales != null && partnership.currSales != null) {
+                    const { admin } = await unauthenticated.admin(partnership.shop);
+                    const response = await admin.graphql(
+                        `#graphql
+                        query queryOrders($id: ID!) {
+                            orders(query: "created_at:2023-10-16") {
+                                edges {
+                                    node {
+                                        discountCodes
+                                        netPaymentSet
+                                        createdAt
+                                    }
+                                }
+                            }
+                        }
+                        `,
+                        {
+                            variables: {
+                            },
+                        }
+                    );
 
-    //                 const {
-    //                     data: { orders },
-    //                 } = await response.json();
+                    const {
+                        data: { orders },
+                    } = await response.json();
 
-    //                 if (orders != null) {
-    //                     let newSales = 0;
-    //                     newSales += orders?.edges?.forEach(function(order) {
-    //                         let sales = 0;
-    //                         for (const code of order.node.discountCodes) {
-    //                             if (code.startsWith("Adelfi")) {
-    //                                 sales = order.node.netPaymentSet.shopMoney.decimal;
-    //                                 break;
-    //                             }
-    //                         }
-    //                         return sales;
-    //                     })
-    //                     partnership.totalSales += newSales;
-    //                     partnership.currSales += newSales;
-    //                 }
-    //                 try {
-    //                     const updateResponse = await db.partnership.updateMany({ where: { shop: partnership.shop }, data: { ...partnership }})
-    //                     if (updateResponse.count === 0) {
-    //                         return json({ error: "Partnership not updated" }, { status: 400 });
-    //                     } else {
-    //                         return json({ success: "Partnership updated" });
-    //                     }
-    //                 } catch (error) {
-    //                     console.error("Error updating partnerships", error);
-    //                     return json({ error: "Internal server error" }, { status: 500 });
-    //                 }
-    //             } else {
-    //                 return json({ error: "No discountId attached to this shop" })
-    //             }
-    //         }));
-    //         return json({updateResponses: updateResponses})
-    //     } else {
-    //         return json({ error: "Invalid token" }, { status: 401 });
-    //     }
-    // }
+                    if (orders != null) {
+                        let newSales = 0;
+                        newSales += orders?.edges?.forEach(function(order) {
+                            let sales = 0;
+                            for (const code of order.node.discountCodes) {
+                                if (code.startsWith("Adelfi")) {
+                                    sales = order.node.netPaymentSet.shopMoney.decimal;
+                                    break;
+                                }
+                            }
+                            return sales;
+                        })
+                        partnership.totalSales += newSales;
+                        partnership.currSales += newSales;
+                    }
+                    try {
+                        const updateResponse = await db.partnership.updateMany({ where: { shop: partnership.shop }, data: { ...partnership }})
+                        if (updateResponse.count === 0) {
+                            return json({ error: "Partnership not updated" }, { status: 400 });
+                        } else {
+                            return json({ success: "Partnership updated" });
+                        }
+                    } catch (error) {
+                        console.error("Error updating partnerships", error);
+                        return json({ error: "Internal server error" }, { status: 500 });
+                    }
+                } else {
+                    return json({ error: "No discountId attached to this shop" })
+                }
+            }));
+            return json({updateResponses: updateResponses})
+        } else {
+            return json({ error: "Invalid token" }, { status: 401 });
+        }
+    }
     return json({ message: "API endpoint reached"});
 };
   
