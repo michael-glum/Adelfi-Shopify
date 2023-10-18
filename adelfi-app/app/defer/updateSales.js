b7a35b4ef8b5f76697197112c3fbb324da6c29d0
michael-glum <glummb@miamioh.edu>
2023-10-17 21:23:14 -0400

    Moved graphql api query from updateSales.js to partnership.js

--- adelfi-app/app/defer/updateSales.js
+++ adelfi-app/app/defer/updateSales.js
@@ -5,81 +5,21 @@ import { json } from "@remix-run/node";
 const API_BASE_URL = "https://adelfi.fly.dev/api";
 
 async function updateSales() {
-    const headers = {
-        "Authorization": `Bearer ${process.env.PRIVATE_AUTH_TOKEN}`
+    const requestBody = {
+        token: `${process.env.PRIVATE_AUTH_TOKEN}`
     }
 
-    const response = await fetch(`${API_BASE_URL}/partnership`, { headers });
-    if (!response.ok) {
-        throw new Error("Failed to fetch partnership data");
-    }
+    const response = await fetch(`${API_BASE_URL}/partnership`, {
+        method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(requestBody)
+    });
 
-    const partnerships = await response.json();
+    const { updateResponses } = await response.json()
 
-    const updateResponses = []
-
-    updateResponses.push(partnerships.forEach(async function(partnership) {
-        let updateResponse = null
-        if (partnership.discountId != null) {
-            const { admin } = await unauthenticated.admin(partnership.shop);
-            const response = await admin.graphql(
-                `#graphql
-                  query queryOrders($id: ID!) {
-                    orders(query: "created_at:2023-10-16") {
-                      edges {
-                        node {
-                          discountCodes
-                          netPaymentSet
-                          createdAt
-                        }
-                      }
-                    }
-                  }
-                `,
-                 {
-                   variables: {
-                   },
-                 }
-            );
-
-            const {
-                data: { orders },
-            } = await response.json();
-
-            if (orders != null) {
-                let newSales = 0;
-                newSales += orders?.edges?.forEach(function(order) {
-                    let sales = 0;
-                    for (const code of order.node.discountCodes) {
-                        if (code.startsWith("Adelfi")) {
-                            sales = order.node.netPaymentSet.shopMoney.decimal;
-                            break;
-                        }
-                    }
-                    return sales;
-                })
-                partnership.totalSales += newSales;
-                partnership.currSales += newSales;
-            }
-
-            const requestBody = {
-                data: partnership,
-                token: `${process.env.PRIVATE_AUTH_TOKEN}`
-            }
-
-            updateResponse = await fetch(`${API_BASE_URL}/partnership`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(requestBody)
-            });
-        }
-
-        return updateResponse;
-    }))
-
-    return updateResponses
+    return json ({ updateResponses })
 }
 
 export default defer(updateSales);
--- adelfi-app/app/routes/api/partnership.js
+++ adelfi-app/app/routes/api/partnership.js
@@ -1,52 +1,90 @@
 import { json } from "@remix-run/node";
 import db from "../../db.server"
+import { unauthenticated } from "../../shopify.server";
 
 const PRIVATE_AUTH_TOKEN = process.env.PRIVATE_AUTH_TOKEN;
 
 export const loader = async ({ request }) => {
-
-    if (request.method === "GET") {
-        const authHeader = request.headers.get("Authorization");
-        if (authHeader) {
-            const token = authHeader.replace("Bearer ", "");
-            if (token === PRIVATE_AUTH_TOKEN) {
-                // Provide relevant partnership data
-                const data = await db.partnership.findMany({
-                    select: {
-                        shop: true,
-                        discountId: true,
-                        totalSales: true,
-                        currSales: true
-                    }
-                });
-                if (!data) {
-                    return json({ message: 'Data not found' }, { status: 404 });
-                }
-                return json(data);
-            } else {
-                return json({ error: "Invalid token" }, { status: 401 });
-            }
-        }
-    } else if (request.method === "POST") {
+    if (request.method === "POST") {
         const { token } = request.locals;
         if (token === PRIVATE_AUTH_TOKEN) {
-            // Update database sales records
-            const { data } = await request.json();
-            try {
-                const updatedPartnerships = await db.partnership.updateMany({ where: { shop: data.shop }, data: { ...data }})
-                if (updatedPartnerships.count === 0) {
-                    return json({ error: "No partnerships were updated" }, { status: 400 });
-                } else {
-                    return json({ success: "Partnerships updated" });
+            const partnerships = await db.partnership.findMany({
+                select: {
+                    shop: true,
+                    discountId: true,
+                    totalSales: true,
+                    currSales: true
                 }
-            } catch (error) {
-                console.error("Error updating partnerships", error);
-                return json({ error: "Internal server error" }, { status: 500 });
+            });
+
+            if (!partnerships) {
+                return json({ message: 'Partnerships not found' }, { status: 404 });
             }
+
+            const updateResponses = []
+
+            updateResponses.push(partnerships.forEach(async function(partnership) {
+                if (partnership.discountId != null && partnership.totalSales != null && partnership.currSales != null) {
+                    const { admin } = await unauthenticated.admin(partnership.shop);
+                    const response = await admin.graphql(
+                        `#graphql
+                        query queryOrders($id: ID!) {
+                            orders(query: "created_at:2023-10-16") {
+                                edges {
+                                    node {
+                                        discountCodes
+                                        netPaymentSet
+                                        createdAt
+                                    }
+                                }
+                            }
+                        }
+                        `,
+                        {
+                            variables: {
+                            },
+                        }
+                    );
+
+                    const {
+                        data: { orders },
+                    } = await response.json();
+
+                    if (orders != null) {
+                        let newSales = 0;
+                        newSales += orders?.edges?.forEach(function(order) {
+                            let sales = 0;
+                            for (const code of order.node.discountCodes) {
+                                if (code.startsWith("Adelfi")) {
+                                    sales = order.node.netPaymentSet.shopMoney.decimal;
+                                    break;
+                                }
+                            }
+                            return sales;
+                        })
+                        partnership.totalSales += newSales;
+                        partnership.currSales += newSales;
+                    }
+                    try {
+                        const updateResponse = await db.partnership.updateMany({ where: { shop: partnership.shop }, data: { ...partnership }})
+                        if (updateResponse.count === 0) {
+                            return json({ error: "Partnership not updated" }, { status: 400 });
+                        } else {
+                            return json({ success: "Partnership updated" });
+                        }
+                    } catch (error) {
+                        console.error("Error updating partnerships", error);
+                        return json({ error: "Internal server error" }, { status: 500 });
+                    }
+                } else {
+                    return json({ error: "No discountId attached to this shop" })
+                }
+            }));
+            return json({updateResponses: updateResponses})
         } else {
             return json({ error: "Invalid token" }, { status: 401 });
         }
     }
-    return json({ message: "Request is valid. API endpoint reached"});
-  };
+    return json({ message: "API endpoint reached"});
+};
   
