032e6599c4ed36330d7d2287426c0fcf63052451
michael-glum <glummb@miamioh.edu>
2023-10-24 16:29:52 -0400

    Defer CRON test

--- adelfi-app/app/defer/updateSales.js
+++ adelfi-app/app/defer/updateSales.js
@@ -19,4 +19,4 @@ async function updateSales() {
     return response
 }
 
-export default defer(updateSales);
+export default defer.cron(updateSales, "35 4 * * *");
--- adelfi-app/app/routes/helloWorld.js
+++ adelfi-app/app/routes/helloWorld.js
@@ -1,8 +1,8 @@
 import updateSales from "~/defer/updateSales";
 
 export const loader = async() => {
-    const response = await updateSales();
-    return new Response(response.id, {
-        status: 200,
-    });
+    //const response = await updateSales();
+    //return new Response(response.id, {
+       // status: 200,
+    //});
 } 
