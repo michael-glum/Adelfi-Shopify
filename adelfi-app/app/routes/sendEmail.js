14b998c85bd6ce9a8e2b7158c729f13effd1ffeb
michael-glum <glummb@miamioh.edu>
2023-10-25 19:13:35 -0400

    Adding email sending capability to discount creation task

--- adelfi-app/app/routes/sendEmail.js
+++ adelfi-app/app/routes/sendEmail.js
@@ -25,7 +25,7 @@ export default async function sendEmail(shop, content, hasAttachment) {
         attachments: [
             {
             filename: shop + "_codes.txt",
-            content: content,
+            content: Buffer.from(content),
             }
         ]
         } : {
